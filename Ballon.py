cc5f2ecbeec20725b172e69ccb68e314d683fe31
Quentin Jacquot <quentin.jacquot@etu.umontpellier.fr>
2025-01-15 13:02:52 +0100

    Modification classe Ballon et ajout d'un main

--- Ballon.py
+++ Ballon.py
@@ -1,20 +1,23 @@
 import numpy as np
+from Graphics import Graphic, SceneRender
+import cv2
+from Tir import Tir
 
 class Ballon :
-    def __init__(self, sprite, idPos, posDebut, posFinal, vitesse, rayon, enContactGant):
+    def __init__(self, sprite, idPos, pos, traj, rayon, enContactGant):
 
         self.sprite = sprite
         self.idPos = idPos
-        self.posDebut = posDebut 
-        self.posFinal = posFinal
-        self.vitesse = vitesse 
+        self.pos = pos
+        self.traj = traj
         self.rayon = rayon
         self.enContactGant = enContactGant
 
     #rafraichi l'image si la balle est en l'air, si elle a touché les gants ou si elle est rentrée dans les cages 
     def update(self):
         if not self.enContactGant :
-            idPos += 1  
+            self.idPos += 1 
+            self.pos = (self.traj[0, self.idPos], self.traj[1, self.idPos], self.traj[2, self.idPos])
             # Changer le sprite 
         elif self.pos[3] > self.posFinal[3] :
             pass
@@ -22,5 +25,10 @@ class Ballon :
         else :
             pass
             # Le ballon est arrété 
+    
+    def get_graphic(self):
+        img = Graphic(cv2.imread(self.sprite, cv2.IMREAD_UNCHANGED))
+        img.resize((self.rayon, self.rayon), cv2.INTER_NEAREST)
+        return img
 
     
--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-print("Hello world!")
+import numpy as np
+from Graphics import Graphic, SceneRender
+import cv2
+from Tir import Tir
+from Ballon import Ballon
+
+def main():
+    EPSILON = 30
+    WIDTH, HEIGHT = 1000, 600
+
+    myTir = Tir()
+
+    # Création du ballon 
+    balle = Ballon(sprite="ballon.png",
+                   idPos=0,
+                   pos=(0,0,0),
+                   traj=myTir.panenka(),
+                   rayon=50,
+                   enContactGant=False)
+    
+    cap = cv2.VideoCapture(0)
+
+    render = SceneRender((WIDTH,HEIGHT))
+
+    while True:
+        caneva = Graphic((WIDTH, HEIGHT))
+        caneva.fill((255, 255, 255))
+
+        render.clear()
+        render.add_layer(caneva)
+        render.add_layer(balle.get_graphic(),balle.update())
+
+        output = render.get_image()
+        cv2.imshow("Resultat", output)
+
+        key = cv2.waitKey(EPSILON) & 0xFF
+        if key == ord('q') or key == 27:
+            break
+
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
