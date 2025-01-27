f31a63d44206aabe798e9945836537d7d762620d
theo.constantin01 <theo.constantin01@etu.umontpellier.fr>
2025-01-27 20:21:13 +0100

    feat; creation fictual leaderboard

--- bdd.py
+++ bdd.py
@@ -1,5 +1,15 @@
 import API_Raspberry as API
 
+"""
+API.delete_all_dweets_for("score")
+
+
+API.dweet_for("score", {"name": "neuer", "score": 1000})
+API.dweet_for("score", {"name": "quentin", "score": 800})
+API.dweet_for("score", {"name": "theo", "score": 600})
+API.dweet_for("score", {"name": "marius", "score": 400})
+API.dweet_for("score", {"name": "tom", "score": 200})"""
+
 # Fonction pour ajouter un score et gérer le leaderboard
 def update_leaderboard(leaderboard, name, score):
     leaderboard.append((name, score))  # Ajouter le nouveau score
